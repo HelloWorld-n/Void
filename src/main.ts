054d4e1fc7c1f6af7de9c8ae7be40bcb1a4e8bc7
HelloWorld-n <gajica.b@gmail.com>
2022-07-26 13:43:21 +0200

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env ts-node
 
+const $sleepDelay = 10
+
 class Main {
 	static sleep(milliseconds: number) {
 		const timeInitial: Date = new Date();
@@ -13,16 +15,29 @@ class Main {
 		for (let item of arg()) {
 			console.clear()
 			console.log(item)
-			this.sleep(10)
+			this.sleep($sleepDelay)
 		}
 	}
 }
 
 if (require.main === module) {
+	const $overflow = 12
+	const $underflow = 3
+	const $step = $sleepDelay / 250
 	Main.main(
 		(function* () {
+			let value = ($overflow + $underflow) / 2
+			let value_add = 1
 			while (true) {
-				yield new Date().toISOString()
+				if (value > $overflow) {
+					value_add = 0 - $step
+				}
+				if (value < $underflow) {
+					value_add = $step
+				}
+				value += value_add
+				
+				yield "\n".repeat(value) + new Date().toISOString()
 			}
 		})
 	)
